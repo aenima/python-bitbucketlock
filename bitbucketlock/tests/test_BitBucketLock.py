3285e691c6d9ad2a1aef9b8f46b4b8931af0ac8d
a.fittkau <alexander.fittkau@gmail.com>
2017-03-24 17:02:53 +0100

    Adding some very basic unit tests.

--- bitbucketlock/tests/test_BitBucketLock.py
+++ bitbucketlock/tests/test_BitBucketLock.py
@@ -3,5 +3,14 @@
 Test suite
 """
 
-# None so far. So sad. 
+from bitbucketlock import BitBucketLock
 
+TEST_HOST = "localhost"
+TEST_USER = "testuser"
+TEST_PASSWORD = "testpassword"
+
+
+def test_BitBucketLock_init():
+    new_instance = BitBucketLock(TEST_HOST, TEST_USER, TEST_PASSWORD)
+    assert isinstance(new_instance, BitBucketLock)
+    return
